eae01607e3cbfac9c149a2fe556d137bc5f7e532
iamSourav37 <souravsrk120@gmail.com>
2020-09-10 11:19:11 +0530

    home page ui(with dummy data) is done

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:developer';
 
 class HomePage extends StatefulWidget {
   @override
@@ -10,19 +11,49 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("My TODO"),
+        title: Text(
+          "ToDo",
+          style: TextStyle(
+            fontSize: 28.0,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
       ),
-      body: ListView.builder(
-        itemCount: 50,
-        itemBuilder: (context, i) {
-          return Card(
-            child: ListTile(
-              leading: Icon(Icons.work),
-              title: Text("My todo app $i"),
-              trailing: Icon(Icons.delete),
-            ),
-          );
-        },
+      body: Container(
+        margin: EdgeInsets.all(5.0),
+        child: ListView.builder(
+          itemCount: 5,
+          itemBuilder: (BuildContext context, int i) {
+            return Card(
+              child: Container(
+                height: 70.0,
+                child: InkWell(
+                  splashColor: Colors.grey[50],
+                  onTap: () {
+                    log("tapped");
+                  },
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      child: Icon(Icons.priority_high),
+                    ),
+                    title: Text("My todo app $i"),
+                    trailing: IconButton(
+                      icon: Icon(
+                        Icons.delete,
+                        color: Colors.lightBlueAccent,
+                      ),
+                      onPressed: () {
+                        log("Delete button pressed");
+                      },
+                    ),
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
