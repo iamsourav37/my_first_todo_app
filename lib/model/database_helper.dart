e12739cd98099b6f9fbcebf9bddd040b25790705
iamSourav37 <souravsrk120@gmail.com>
2020-09-14 14:03:01 +0530

    CRUD method are defined in database_helper file

--- lib/model/database_helper.dart
+++ lib/model/database_helper.dart
@@ -30,7 +30,7 @@ class DatabaseHelper {
 // getter for getting the database
   Future<Database> get database async {
     if (_database == null) {
-      this.initDatabase();
+      _database = await this.initDatabase();
     }
     return _database;
   }
@@ -38,11 +38,53 @@ class DatabaseHelper {
   Future<Database> initDatabase() async {
     Directory directory = await getApplicationSupportDirectory();
     String path = join(directory.path, tableName);
-    return openDatabase(path, version: dbVersion, onCreate: _onCreate);
+    return await openDatabase(path, version: dbVersion, onCreate: _onCreate);
   }
 
   _onCreate(Database db, int version) async {
-    db.execute(
+    await db.execute(
         'CREATE TABLE $tableName($colId INTEGER PRIMARY KEY,$colTitle TEXT,$colDescription TEXT, $colDate TEXT, $colPriority INTEGER)');
+  }
+  // database and table is created
+
+  // now performing the CRUD operation
+
+  // fetching or read the values from the database
+  Future<List<Map<String, dynamic>>> getNoteMapList() async {
+    Database db = await this.database;
+    var result = await db.query(tableName, orderBy: '$colPriority ASC');
+    // returning a list of map object List<Map<String,dynamic>> in this format, later we convert it to List of Note List<Note> object to display on the screen
+    return result;
+  }
+
+  // insert a note to the database
+  Future<int> insertNote(Note note) async {
+    Database db = await this.database;
+    var result = await db.insert(tableName,
+        note.toMap()); // if this line execute successfully then returns 1 if failed then returned 0. thats why return type is Future<int>
+    // sqflite plugins deals with map object, so convert it to a map before inserting
+    return result;
+  }
+
+  // update a note
+  Future<int> updateNote(Note note) async {
+    Database db = await this.database;
+    var result = await db.update(tableName, note.toMap(),
+        where: '$colId = ?', whereArgs: [note.id]);
+    return result;
+  }
+
+  Future<int> deleteNote(int deleteId) async {
+    Database db = await this.database;
+    var result =
+        await db.rawDelete('DELETE FROM $tableName WHERE $colId = $deleteId');
+    return result;
+  }
+
+  Future<int> getCount() async {
+    Database db = await this.database;
+    int count =
+        Sqflite.firstIntValue(await db.rawQuery('SELECT COUNT(*) FROM Test'));
+    assert(count == 2);
   }
 }
